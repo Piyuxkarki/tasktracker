9185f1c169f9b569f30a2069413dda66dae5b78e
Piyush Karki <piyushkarki2003@gmail.com>
2023-12-25 15:04:50 +0530

    initial commit

---
+++ Sign Up Page/script.js
@@ -0,0 +1,93 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+  
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+
+})(jQuery);
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the sign-up button
+    var signUpButton = document.getElementById("signup-button");
+    
+    // Get a reference to the form
+    var signUpForm = document.getElementById("signup-form");
+
+    // Add a click event listener to the button
+    signUpButton.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent the default form submission
+        
+        // Check if the form is valid before redirecting
+        if (signUpForm.checkValidity()) {
+            // Redirect to the login page
+            window.location.href = "LogIn.html";
+
+        if (GetStarted.checkValidity()){
+          window.location.href="SignUp.html"
+        }
+        }
+    });
+});
